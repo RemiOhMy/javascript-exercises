7f36749538c8967add17090f8aef60f880e79f39
Remi <remiohmy@gmail.com>
2022-12-09 12:15:54 -0500

    complete ex 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (repeatWord, repeatNum) {
+    let repeatStr = "";
+    if (repeatNum < 0) {
+        repeatStr = "ERROR";
+    }
+    else {
+        for (i = 0; i < repeatNum; i++) {
+            repeatStr += repeatWord;
+        }
+    }
+    return repeatStr;
 };
 
 // Do not edit below this line
