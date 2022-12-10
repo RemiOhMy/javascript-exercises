908977974a7c3f1289dc72e44aee9909618e7099
Remi <remiohmy@gmail.com>
2022-12-10 12:26:36 -0500

    complete ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    let smallNum, bigNum, allSum = 0;
+    if (Number.isInteger(numOne) && Number.isInteger(numTwo) && numOne > 0 && numTwo > 0){
+        if (numOne < numTwo) {
+            smallNum = numOne;
+            bigNum = numTwo;
+        }
+        else {
+            smallNum = numTwo;
+            bigNum = numOne;
+        }
+        for (let i = smallNum; i <= bigNum; i++){
+            allSum += i;
+        }
+        return allSum;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
