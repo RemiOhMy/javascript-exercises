6e537f625ee32ecdc61b2f7bd0575542a20a306d
Remi <remiohmy@gmail.com>
2022-12-10 12:03:38 -0500

    complete ex 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(items, ...elementsToRemove) {
+    const updatedArray = Array.from(items);
+    const rmvElements = Array.from(elementsToRemove);
+    for (let i = 0; i < rmvElements.length; i++) {
+        for (let a = 0; a < updatedArray.length; a++) {
+            if (updatedArray[a] === rmvElements[i]) {
+                updatedArray.splice(a, 1);
+            }
+        }
+    }
+    return updatedArray;
 };
 
 // Do not edit below this line
